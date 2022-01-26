5441a371a79676873d13498cdd5e03efa8f074a4
Guilherme <guilherme.alves96@hotmail.com>
2022-01-26 19:23:52 -0300

    Filtrando dados do csv

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from httpx import head
 import pandas as pd
 #import numpy as np
 #import seaborn as sns
@@ -6,7 +7,21 @@ import pandas as pd
 
 """Exploração dos dados"""
   
-base_credit = pd.read_csv('credit_data.csv', index_col='clientid').head(7)
+base_credit = pd.read_csv('credit_data.csv', index_col='clientid')
 
+# É para trazer as 5 primeiras linhas
+#base_credit = base_credit.head()
+
+# É para trazer as 5 ultimas linhas
+#base_credit = base_credit.tail()
+
+# Retorna a descrição dos dados numéricos do DataFrame
+base_credit = base_credit.describe()
+print(base_credit)
+
+""" Filtrando """
+# base_credit = base_credit[base_credit['income'] >= 66952.688845] 
+
+# base_credit = base_credit[base_credit['loan'] <= 1.377630]
 
 print(base_credit)
